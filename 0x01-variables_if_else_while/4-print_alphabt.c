e9cebfb5f9d3d7c5d4d72a036ee8d58eb7717e95
Famous2 <Famousetsekpimi@yahoo.com>
2023-03-18 02:36:20 -0700

    Remove deprecated code to reduce technical debt

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
 /**
- * main - prints the alphabet in lowercase 
- * followed by a new line, except q and e
- * Return: Always 0
+ * main - Entry point
+ *Description: Prints the alphabet in lowercase except q and e
+ *Return: Always 0 (Success)
  */
 int main(void)
-{char la;
-for (la = 'a'; la <= 'z'; la++)
 {
-if (la != 'e' && la != 'q')
-putchar(la);
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++) /* Loop through a to z */
+{
+if (letter != 'q' && letter != 'e') /* Check if letter is q or e */
+{
+putchar(letter); /* Print the letter */
 }
-putchar('\n');
-return (0);
+}
+putchar('\n'); /* Print a new line */
+return (0); /* Always return 0 to indicate success */
 }
