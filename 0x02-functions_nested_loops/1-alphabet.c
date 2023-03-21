037b4cfc383da7df129125c31c5c381fb4737567
Famous2 <Famousetsekpimi@yahoo.com>
2023-03-21 15:00:00 -0700

    Add new feature to improve user experiences

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * print_alphabet - Prints all alphabet lowercase
  */
@@ -7,6 +6,6 @@ void print_alphabet(void)
 {
 char letter;
 for (letter = 'a'; letter <= 'z'; letter++)
-_putchar(letter);	
+_putchar(letter);
 _putchar('\n');
 }
